e385fb7aa2bdf2f7e81e7b8a5eaf153357c721bb
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2024-06-05 10:30:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import lark from "@larksuiteoapi/node-sdk";
 import dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
 import fetch from "node-fetch";
 import showdown from "showdown";
 
@@ -20,27 +19,6 @@ const client = new lark.Client({
   disableTokenCache: false,
   domain: lark.Domain.Lark,
 });
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Database connected successfully"))
-  .catch((err) => console.log(err));
-
-const eventSchema = new mongoose.Schema({
-  event_id: { type: String, unique: true },
-  content: String,
-});
-
-const msgSchema = new mongoose.Schema({
-  sessionId: String,
-  question: String,
-  answer: String,
-  msgSize: Number,
-  createdAt: { type: Date, default: Date.now },
-});
-
-const Event = mongoose.model("Event", eventSchema);
-const Msg = mongoose.model("Msg", msgSchema);
 
 app.use(express.json());
 
@@ -81,57 +59,6 @@ async function reply(messageId, content) {
   }
 }
 
-async function buildConversation(sessionId, question) {
-  const prompt = [];
-  const messages = await Msg.find({ sessionId })
-    .sort({ createdAt: 1 })
-    .limit(5);
-  messages.forEach((row) => {
-    prompt.push({ role: "user", content: row.question });
-    prompt.push({ role: "assistant", content: row.answer });
-  });
-  prompt.push({ role: "user", content: question });
-  return prompt;
-}
-
-async function saveConversation(sessionId, question, answer) {
-  try {
-    if (!question || !answer) {
-      throw new Error("Question or answer is missing or empty.");
-    }
-    const msgSize = question.length + answer.length;
-    await new Msg({ sessionId, question, answer, msgSize }).save();
-    await discardConversation(sessionId);
-  } catch (error) {
-    logger("Error saving conversation:", error);
-    throw error; // Propagate the error further if needed
-  }
-}
-
-async function discardConversation(sessionId) {
-  const messages = await Msg.find({ sessionId }).sort({ createdAt: -1 });
-  let totalSize = 0;
-  const countList = [];
-
-  messages.forEach((row) => {
-    totalSize += row.msgSize;
-    countList.push({
-      msgId: row._id,
-      totalSize,
-    });
-  });
-
-  for (const c of countList) {
-    if (c.totalSize > 1024) {
-      await Msg.deleteOne({ _id: c.msgId });
-    }
-  }
-}
-
-async function clearConversation(sessionId) {
-  await Msg.deleteMany({ sessionId });
-}
-
 async function cmdHelp(messageId) {
   const helpText = `Lark GPT manpages
 
@@ -143,7 +70,6 @@ Usage:
 }
 
 async function cmdClear(sessionId, messageId) {
-  clearConversation(sessionId);
   await reply(messageId, "✅ All history removed");
 }
 
@@ -209,30 +135,18 @@ async function doctor() {
   };
 }
 
-async function handleReply(userInput, sessionId, messageId, eventId) {
+async function handleReply(userInput, sessionId, messageId) {
   const question = userInput.text.replace("@_user_1", "").trim();
   logger("question: " + question);
   const action = question.trim();
   if (action.startsWith("/")) {
     return await cmdProcess({ action, sessionId, messageId });
   }
-  const prompt = await buildConversation(sessionId, question);
-  const openaiResponse = await query({ question: prompt });
+  const openaiResponse = await query({ question });
 
   const answer = openaiResponse.text;
-  await saveConversation(sessionId, question, answer);
   await reply(messageId, answer);
 
-  try {
-    await new Event({ event_id: eventId, content: userInput.text }).save();
-  } catch (error) {
-    if (error.code === 11000) {
-      logger("Duplicate event ID:", eventId);
-      return { code: 1, message: "Duplicate event ID" };
-    } else {
-      throw error;
-    }
-  }
   return { code: 0 };
 }
 
@@ -266,23 +180,6 @@ app.post("/webhook", async (req, res) => {
     const senderId = params.event.sender.sender_id.user_id;
     const sessionId = chatId + senderId;
 
-    const event = await Event.findOne({ event_id: eventId });
-    if (event) {
-      logger("skip repeat event");
-      return res.json({ code: 1 });
-    }
-
-    try {
-      await new Event({ event_id: eventId }).save();
-    } catch (error) {
-      if (error.code === 11000) {
-        logger("Duplicate event ID:", eventId);
-        return res.json({ code: 1, message: "Duplicate event ID" });
-      } else {
-        throw error;
-      }
-    }
-
     if (params.event.message.chat_type === "p2p") {
       if (params.event.message.message_type != "text") {
         await reply(messageId, "Not support other format question, only text.");
@@ -290,23 +187,13 @@ app.post("/webhook", async (req, res) => {
         return res.json({ code: 0 });
       }
       const userInput = JSON.parse(params.event.message.content);
-      const result = await handleReply(
-        userInput,
-        sessionId,
-        messageId,
-        eventId
-      );
+      const result = await handleReply(userInput, sessionId, messageId);
       return res.json(result);
     }
 
     if (params.event.message.chat_type === "group") {
       const userInput = JSON.parse(params.event.message.content);
-      const result = await handleReply(
-        userInput,
-        sessionId,
-        messageId,
-        eventId
-      );
+      const result = await handleReply(userInput, sessionId, messageId);
       return res.json(result);
     }
   }
